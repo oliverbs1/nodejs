ef9fd5a217e7898033bd30d98798dc94778997ae
oliver bsaibes <oliverbsaibes@gmail.com>
2016-11-03 16:41:46 +0100

    fist commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('https'),
+    port = 6001;
+
+function handle(req, res) {
+    console.log('requete');
+    res.end('<!DOCTYPE html><html><head><title>blabla</title></head><body><h1>hello</h1></body></html>');
+}
+var server = http.createServer(handle);
+server.listen(port, function () {
+    console.log("Server listening on: http://localhost:%s", port);
+})
