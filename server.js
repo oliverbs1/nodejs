0f0f8e596f916eeb7b085cf68de152093c1024b4
oliver bsaibes <oliverbsaibes@gmail.com>
2016-11-03 16:56:20 +0100

    commit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var http = require('https'),
+var http = require('http'),
     port = 6001;
 
 function handle(req, res) {
     console.log('requete');
-    res.end('<!DOCTYPE html><html><head><title>blabla</title></head><body><h1>hello</h1></body></html>');
+    res.end('<!doctype html><html><head><title>blabla</title></head><body><h1>hello</h1></body></html>');
 }
 var server = http.createServer(handle);
 server.listen(port, function () {
